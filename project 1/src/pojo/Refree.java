b3edfd136efa32ee965e69a93a0d41f41446864a
shubham <shubg13@gmail.com>
2018-07-10 15:39:09 +0530

    class of football

---
+++ project 1/src/pojo/Refree.java
@@ -0,0 +1,32 @@
+package pojo;
+
+public class Refree {
+	
+	private String name;
+	private String nationality;
+	private String age;
+	
+	public void conductsGame() {
+		System.out.println("conducts");
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNationality() {
+		return nationality;
+	}
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	
+
+}
